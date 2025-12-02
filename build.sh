6040820db7150a54458d2c91290266944e0a65af
jinxin <jinxin70@gmail.com>
2025-12-02 09:52:08 +0800

    feat: Refactor OS image creation in build script using `dd` for padding and update QEMU launch command.

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ nasm -f bin boot.asm -o boot.bin
 x86_64-elf-gcc -m32 -ffreestanding -nostdlib -c kernel.c -o kernel.o
 x86_64-elf-ld -m elf_i386 -T linker.ld -o kernel.elf kernel.o
 x86_64-elf-objcopy -O binary kernel.elf kernel.bin
-cat boot.bin kernel.bin > os.img
+
 # 填充镜像到至少11个扇区（5632字节），确保有足够数据供bootloader读取
 dd if=/dev/zero of=os.img bs=512 count=11 conv=notrunc 2>/dev/null
 dd if=boot.bin of=os.img bs=512 count=1 conv=notrunc 2>/dev/null
 dd if=kernel.bin of=os.img bs=512 seek=1 conv=notrunc 2>/dev/null
-qemu-system-i386 -hda os.img
+# qemu-system-i386 -hda os.img
+qemu-system-i386 -drive file=os.img,format=raw,if=ide
